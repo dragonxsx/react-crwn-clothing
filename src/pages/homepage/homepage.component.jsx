f5c28ed4be75671e796c7f4683e34be4606dd9a7
Long Nguyen <long_nguyen@epam.com>
2021-12-05 11:10:51 +0700

    Homepage and Directory component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+export const Homepage = () => {
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
