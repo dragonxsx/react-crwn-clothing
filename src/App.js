f320ec32364a6e845a4cf28fd9e46cd7a7855b00
Long Nguyen <long_nguyen@epam.com>
2021-12-07 15:37:20 +0700

    Listening to Firebase authentication state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Header from './components/header/header.component';
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/shop' element={<ShopPage />} />
-        <Route path='/signin' element={<SignInAndSignUpPage />} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/shop' element={<ShopPage />} />
+          <Route path='/signin' element={<SignInAndSignUpPage />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
